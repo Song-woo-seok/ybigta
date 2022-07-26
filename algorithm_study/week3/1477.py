5e9f230c0c3b1e9ee774fca71bbd94b9889c3768
uuu_suk_ <7633636@gmail.com>
2022-07-26 16:17:53 +0900

    study

--- algorithm_study/week3/1477.py
+++ algorithm_study/week3/1477.py
@@ -1,17 +1,24 @@
 import sys
 n, m, l = map(int, sys.stdin.readline().split())
 a = list(map(int,sys.stdin.readline().split()))
-start, end = 1, max(a)
+a.append(l)
+a.sort()
 
-while start <= end:
-    mid = (start + end) // 2
-    station = 0 
+left, right = 1, l-1
+answer = 0
+
+while left<=right:
+    mid = (left+right)//2
+    current, cnt = 0, 0
+
     for i in a:
-        if i>mid:
-            station += i - mid 
-        
-    if station >= m: 
-        start = mid + 1
-    elif station < m:
-        end = mid - 1
-print(end)
+        diff = i - current
+        current = i
+        if diff > mid: # 최대 거리보다 크므로 지어야함
+            cnt += (diff-1)//mid
+    if cnt > m: # m보다 많이 지음. 너무 가까이 지음
+        left = mid+1
+    else: # m보다 작거나 같게 지음. 너무 멀리 지음
+        right = mid-1
+        answer = mid 
+print(answer)
